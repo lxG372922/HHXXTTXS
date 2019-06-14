4c3e4ddc0ff816ed8bae2d8322705e40026618e1
马亚恒 <240068702@qq.com>
2019-06-14 18:18:01 +0800

    提交

---
+++ HHXXTTXS/WriteCode/COCMine/V/MyFooterView.h
@@ -0,0 +1,23 @@
+//
+//  MyFooterView.h
+//  NewStock
+//
+//  Created by mac on 2019/6/14.
+//  Copyright © 2019 class. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyFooterView : UIView
+
+@property (nonatomic , strong)UIButton *outBtn;
+
+@property (nonatomic , strong)UIView *bgView;
+
+@property (nonatomic , strong)UILabel *lab;
+
+@end
+
+NS_ASSUME_NONNULL_END
