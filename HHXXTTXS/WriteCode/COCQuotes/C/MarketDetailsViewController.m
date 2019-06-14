2556a1e9aaec0152e8ec8a9aa0d0e2c7a365c3a8
xujiahuicoin <xujiahuicoin@gmail.com>
2019-06-14 19:25:32 +0800

    xu：行情

---
+++ HHXXTTXS/WriteCode/COCQuotes/C/MarketDetailsViewController.m
@@ -0,0 +1,28 @@
+//
+//  MarketDetailsViewController.m
+//  HHXXTTXS
+//
+//  Created by mac on 2019/6/14.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import "MarketDetailsViewController.h"
+
+@interface MarketDetailsViewController ()
+
+@end
+
+@implementation MarketDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = RGBColor(29, 36, 51);
+    self.title = @"";
+    
+}
+
+
+
+
+@end
---
+++ HHXXTTXS/WriteCode/COCQuotes/M/MarketlistCodeAllHX.m
@@ -0,0 +1,13 @@
+//
+//  MarketlistCodeAllHX.m
+//  HHXXTTXS
+//
+//  Created by mac on 2019/6/14.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import "MarketlistCodeAllHX.h"
+
+@implementation MarketlistCodeAllHX
+
+@end
