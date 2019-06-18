453e3cb3393f5a263254ccca0322dcbf697e7013
xujiahuicoin <xujiahuicoin@gmail.com>
2019-06-18 09:29:41 +0800

    xu;

--- HHXXTTXS/WriteCode/COCQuotes/C/MarketDetailsViewController.h
+++ HHXXTTXS/WriteCode/COCQuotes/C/MarketDetailsViewController.h
@@ -12,7 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MarketDetailsViewController : COCBaseViewController
 @property(nonatomic,strong) MarketModelHX * marketmodel ;
+///名字
 @property(nonatomic,copy) NSString * marketName;
+
 @end
 
 NS_ASSUME_NONNULL_END
